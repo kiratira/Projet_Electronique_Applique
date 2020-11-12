16300c6ae409aea26d11abc33ad6624fd63eedb4
Youri Iacono <47464326+Dvalnir@users.noreply.github.com>
2020-11-12 16:11:33 +0100

    Clean up et doc
    Clean up de certains morceaux de codes (espace entre les symboles, retour lignes après fonctions, ...) et documentation de ce que font une partie des fonctions.

--- EEPROM.c
+++ EEPROM.c
@@ -1,5 +1,5 @@
 /*
- * File:   newmain2.c
+ * File:   EEPROM.c
  * Author: capal
  *
  * Created on 29 octobre 2020, 8:33
@@ -29,28 +29,48 @@
 
 //------------------------------ FONCTION ECRITURE EEPROM ------------------------------------------------------------------------------------------------------------------------------------------
 
+// Ecris une donnee a l'adresse x de l'EEPROM
 void Ecriture_EEPROM(unsigned char adresse, unsigned char data);
-void Stockage_EEPROM( unsigned char annee ,unsigned char mois , unsigned char jour  , unsigned char heure, unsigned char minute,unsigned char seconde,  unsigned char partie_entiere_survitesse, unsigned char partie_decimal_survitesse);
+
+// Ecris un enregistrement du radar dans l'EEPROM (utilise des donn�e individuelle)
+void Stockage_EEPROM(
+        unsigned char annee,
+        unsigned char mois,
+        unsigned char jour,
+        unsigned char heure,
+        unsigned char minute,
+        unsigned char seconde,
+        unsigned char partie_entiere_survitesse,
+        unsigned char partie_decimal_survitesse
+);
+
+// Ecris un enregistrement du radar dans l'EEPROM (utilise un tableau contenant les 8 donnees necessaire)
 void Stockage_TAB_EEPROM(unsigned char TAB[8],unsigned char NB_elem);
 
 //------------------------------ FONCTION LECTURE EEPROM -------------------------------------------------------------------------------------------------------------------------------------------
 
+// Lis la donnee qui se trouve a l'adresse x de l'EEPROM
 unsigned char Lecture_E(unsigned char adresse);
+// TODO
 void Lecture_EEPROM(unsigned char N, unsigned char Tab_transition [8]);
+// TODO
 unsigned char Size_EEPROM();
 
 //------------------------------ FONCTION DE SUPPRESSION EEPROM -------------------------------------------------------------------------------------------------------------------------------------------
 
+// TODO
 void Supp_ALL_EEPROM();
+// TODO
 void Lecture_donnees_supp();
+// TODO
 void Supp_data_EEPROM(unsigned char p);
 
 //------------------------------ DECLARATION VARIABLE EN RAM -------------------------------------------------------------------------------------------------------------------------------------------
 
 unsigned char Tab_data_transition[8] ;
 unsigned char Tab_Adresse_supp[10];
-unsigned char N = 0; //nbre d'enregistremet
-unsigned char suppression = 0;
+unsigned char N = 0; //nombre d'enregistrement
+unsigned char suppression = 0; 
 unsigned char x = 0; //nombre de valeur supprimer
 
 //------------------------------ DECLARATION / FONCTION AUTRE  -------------------------------------------------------------------------------------------------------------------------------------------
@@ -60,32 +80,46 @@ void affichage(unsigned char adresse, unsigned char data); //affichage 7 segment
 unsigned char pos_segment[16]={0b01000100,0b11110101,0b10001100,0b10100100,0b00110101,0b00100110,0b00000110,0b11110100,0b00000100,0b00100100,0b00010100,0b00000111,0b01001110,0b10000101,0b00001110, 0b00011110};
 //(0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F)
 unsigned char adresse_segment[4] = {0b01000000,0b01000110,0b01001110,0b01000010};
-//(SEG1 , SEG2, SEG3 , SEG4)    
+//(SEG1 , SEG2, SEG3 , SEG4)
 
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 void main(void) 
 {
-    //eeprom
-    
+    // Config de l'EEPROM
+    // bit 7 EEPGD : 1 = Accesses program memory
+    // bit 6-4 : Unimplemented  Read as '0'
+    // bit 3 WRERR : 0 =  The write operation completed
+    // bit 2 WREN : 1 = Allows write cycles
+    // bit 1 WR : 0 =  Write cycle to the EEPROM is complete
+    // bit 0 RD : 0 =  Does not initiate an EEPROM read
     EECON1 = 0b1000100;
     
     //7 segments et I²C
     
-    SSPSTAT = 0b10000000; //initialisation des ssps
+    SSPSTAT = 0b10000000; // Initialisation des SSPS
     SSPCON = 0b00101000;
     SSPADD = 49;
     SSPCON2 = 0;
     
     //Initialisation
     
-    N = Lecture_E(1); //lire dans l'EEPROM La valeur a l'adresse 1 qui correspond a notre nombre d'enregistrement
+    N = Lecture_E(1); // lire dans l'EEPROM La valeur a l'adresse 1 qui correspond a notre nombre d'enregistrement
     suppression = Lecture_E(2); // lire l'EERPOM si la il y a eu une suppresion
-    Lecture_donnees_supp(); // on lit toute l'EEPROM pour voir si des data on été supp
+    Lecture_donnees_supp(); // on lit toute l'EEPROM pour voir si des data on ete supprime
     
     //tableau de test 
-    unsigned char tableau[8] = {pos_segment[0],pos_segment[1],pos_segment[2],pos_segment[3],pos_segment[4],pos_segment[5],pos_segment[6],pos_segment[7]};
+    unsigned char tableau[8] = {
+        pos_segment[0],
+        pos_segment[1],
+        pos_segment[2],
+        pos_segment[3],
+        pos_segment[4],
+        pos_segment[5],
+        pos_segment[6],
+        pos_segment[7]
+    };
     
-    // ON COMMENCE LA BOUCLE 
+    // ON COMMENCE LA BOUCLE
     while(1)
     {        
         ;
@@ -112,6 +146,7 @@ void Ecriture_EEPROM(unsigned char adresse, unsigned char data)
     EECON1bits.WREN = 0;
     while(EECON1bits.WR);
 }
+
 void Stockage_EEPROM(unsigned char annee ,unsigned char mois , unsigned char jour  , unsigned char heure, unsigned char minute,unsigned char seconde,  unsigned char partie_entiere_survitesse, unsigned char partie_decimal_survitesse)
 {
     if(suppression==1) // si faut remplacer une data effacer
@@ -149,29 +184,29 @@ void Stockage_EEPROM(unsigned char annee ,unsigned char mois , unsigned char jou
     N++;
     Ecriture_EEPROM(1,N);
 }
+
 void Stockage_TAB_EEPROM(unsigned char TAB[8],unsigned char NB_elem) // TAB[8] = {int annee ,int mois , int jour  , int heure, int minute,int seconde,  int partie_entiere_survitesse, int partie_decimal_survitesse};
 {
     if(suppression==1) // si faut remplacer une data effacer
     {
         if(Tab_Adresse_supp[x] != 0xff)
         {
-            for(unsigned char i =0 ; i<NB_elem ; i++)
+            for(unsigned char i = 0; i < NB_elem; i++)
             {
                 Ecriture_EEPROM(10+i+x*8 , TAB[i]);
             }
         
             Tab_Adresse_supp[x] = 0xff; //remplace la valeur du tableau a la position de la dernière suppression par 0xff
-            x-- ; //déincrement la pos dans le tableau
+            x-- ; //decremente la pos dans le tableau
         
         }
     }
     else
-    {//Ecris les données dans l'EEPROM suivant le tableau d'adressage
+    {//Ecris les donnees dans l'EEPROM suivant le tableau d'adressage
         for(unsigned char i =0 ; i<NB_elem ; i++)
         {
             Ecriture_EEPROM(10+i+N*8 , TAB[i]);
-
-        }  
+        }
     }// incremente le Nombre d'enregistrement
     N++;
     Ecriture_EEPROM(1,N); 
@@ -181,18 +216,20 @@ void Stockage_TAB_EEPROM(unsigned char TAB[8],unsigned char NB_elem) // TAB[8] =
 
 unsigned char Lecture_E(unsigned char adresse)
 {
-    EEADR =adresse;
+    EEADR = adresse;
     EECON1bits.EEPGD = 0;
-    EECON1bits.RD =1; 
+    EECON1bits.RD = 1; 
     return EEDATA; 
 }
-void Lecture_EEPROM(unsigned char N, unsigned char Tab_transition [8]) // envoie les donné dans le tableau de transition de données
+
+void Lecture_EEPROM(unsigned char N, unsigned char Tab_transition [8]) // envoie les donnee dans le tableau de transition de donnees
 {
-    for(unsigned char i = 0 ; i<8 ; i++)
+    for(unsigned char i = 0; i < 8; i++)
     {
-        Tab_transition [i] = Lecture_E(10+ i +N*8);
+        Tab_transition [i] = Lecture_E(10 + i + N*8);
     }
 }
+
 unsigned char Size_EEPROM() //nombre d'enregistrement effectuer
 {  
     unsigned char i = 0;
@@ -219,6 +256,7 @@ void Supp_ALL_EEPROM() //Clear l'eeprom
    Ecriture_EEPROM(1,0);
    N = 0;
 }
+
 void Supp_data_EEPROM(unsigned char p)
 {
     // ici faut verifier si le tableau d'adresse supp est complet ?
@@ -242,7 +280,8 @@ void Supp_data_EEPROM(unsigned char p)
         //retour d'erreur
         affichage(adresse_segment[1],pos_segment[16]); //ici affiche F sur le 1er 7 segments si tableau plein
     }
-} 
+}
+
 void Lecture_donnees_supp()
 {
     if(suppression == 1)
@@ -265,6 +304,7 @@ void Lecture_donnees_supp()
 }
 
 //------------------------------ FONCTION AUTRE  -------------------------------------------------------------------------------------------------------------------------------------------
+
 void affichage(unsigned char adresse, unsigned char data)
 {
     PIR1bits.SSPIF = 0;
