388ddb4ac8692e8557fb171feecacc63df0fc4dc
Youri Iacono <47464326+Dvalnir@users.noreply.github.com>
2020-11-18 23:54:00 +0100

    Gros refactoring
    Changement du nom des fonctions min10 et min60 vers get_unit et get_dizaine.
    Changement des int en char.
    Cleaning du fichier

--- RTC.c
+++ RTC.c
@@ -1,5 +1,5 @@
 /*
- * File:   Cours1.c
+ * File:   RTC.c
  * Author: Miki
  *
  * Created on 22 septembre 2020, 14:00
@@ -21,24 +21,21 @@
 // #pragma config statements should precede project file includes.
 // Use project enums instead of #define for ON and OFF.
 
-
-
 #include <xc.h>
 #define _XTAL_FREQ 10000000
 
+
 // DECLARATION PROTOTYPES FONCTIONS
-void affichage(unsigned int adresse, unsigned int data);
-void RTC_Send(int adresse, int regis ,int data);
-int RTC_Recieve(int adresse, int regis);
+void affichage(unsigned char adresse, unsigned char data);
+void RTC_Send(char adresse, char regis ,char data);
+char RTC_Recieve(char adresse, char regis);
 char Init_RTC (char secondes, char minutes, char heures, char jour, char mois, char annee);
 void Read_RTC(char Tab[6]);
-char min10(char data);
-char min60(char data);
+char get_unit(char data);
+char get_dizaine(char data);
 
 
-
-
-void main(void) 
+void main(void)
 {
     //char cligno = 0;
     CMCON = 7;              //Desactivation des comparateurs
@@ -52,117 +49,147 @@ void main(void)
     SSPCON = 0b00101000;
     SSPADD = 39;
     SSPCON2 = 0;
-    
+
     TRISC = 255;
     TXSTA = 0;
     RCSTA = 0b10000000;
     SPBRG = 15;
+
     // DEFINITION AFFICHAGE 7SEGMENTS POSITIONS + ADRESSES
-    int pos_segment[16]={0b01000100,0b11110101,0b10001100,0b10100100,0b00110101,0b00100110,0b00000110,0b11110100,0b00000100,0b00100100,0b00010100,0b00000111,0b01001110,0b10000101,0b00001110, 0b00011110};
-    int adresses[4] = {0b01000010, 0b01000110, 0b01000000, 0b01001110};
-    
+    char pos_segment[16]=
+    {
+        0b01000100,
+        0b11110101,
+        0b10001100,
+        0b10100100,
+        0b00110101,
+        0b00100110,
+        0b00000110,
+        0b11110100,
+        0b00000100,
+        0b00100100,
+        0b00010100,
+        0b00000111,
+        0b01001110,
+        0b10000101,
+        0b00001110,
+        0b00011110
+    };
+
+    char adresses[4] =
+    {
+        0b01000010,
+        0b01000110,
+        0b01000000,
+        0b01001110
+    };
+
     //AFFICHAGE DE BASE SUR LES 7SEGMENTS
     affichage(adresses[0], pos_segment[0]);
     affichage(adresses[1], pos_segment[0]);
     affichage(adresses[2], pos_segment[0]);
     affichage(adresses[3], pos_segment[0]);
+
     //INITIALISATION DU TEMPS 0 SUR LA RTC_
-    Init_RTC(0,0,0,0,0,0);
+    Init_RTC(0, 0, 0, 0, 0, 0);
+
     // INITIALISATION TABLEAU POUR TRANSFERT ET STOCKAGE DES DONNEES 
-    char Tab[6]= {0,0,0,0,0,0};
+    char Tab[6]= {0, 0, 0, 0, 0, 0};
     
     //BOUCLE INFINIE POUR LECTURE ET AFFICHAGE
     while(1)
     {
         Read_RTC(Tab);
-        
-        affichage(adresses[3], pos_segment[min10(Tab[5])]);
-        affichage(adresses[2], pos_segment[min60(Tab[5])]);
-        affichage(adresses[1], pos_segment[min10(Tab[4])]);
-        affichage(adresses[0], pos_segment[min60(Tab[4])]);
+
+        affichage(adresses[3], pos_segment[get_unit(Tab[5])]);
+        affichage(adresses[2], pos_segment[get_dizaine(Tab[5])]);
+
+        affichage(adresses[1], pos_segment[get_unit(Tab[4])]);
+        affichage(adresses[0], pos_segment[get_dizaine(Tab[4])]);
         PORTBbits.RB4 = !PORTBbits.RB4;     // LED clignottage
-    
-        __delay_ms(500);      
+
+        __delay_ms(500);
     }
    return;
 }
 
-void affichage(unsigned int adresse, unsigned int data)
+void affichage(unsigned char adresse, unsigned char data)
 {
     PIR1bits.SSPIF = 0;
     SSPCON2bits.SEN = 1;        //START
     while(!PIR1bits.SSPIF){}
-    
+
     PIR1bits.SSPIF = 0;
     SSPBUF = adresse;           //Adress sending
     while(!PIR1bits.SSPIF){}
-    
+
     PIR1bits.SSPIF = 0;
     SSPBUF = data;              //Data sending
     while(!PIR1bits.SSPIF){}
-    
+
     PIR1bits.SSPIF = 0;
     SSPCON2bits.PEN = 1;        //Stop
     while(!PIR1bits.SSPIF){}
 }
 
-void RTC_Send(int adresse, int regis ,int data)
+void RTC_Send(char adresse, char regis, char data)
 {
     PIR1bits.SSPIF = 0;
     SSPCON2bits.SEN = 1;        //START
     while(!PIR1bits.SSPIF){}
-    
+
     PIR1bits.SSPIF = 0;
     SSPBUF = adresse;           //Adress sending
     while(!PIR1bits.SSPIF){}
-    
+
     PIR1bits.SSPIF = 0;
-    SSPBUF = regis;           //Registre sending
+    SSPBUF = regis;             //Registre sending
     while(!PIR1bits.SSPIF){}
-    
+
     PIR1bits.SSPIF = 0;
     SSPBUF = data;              //Data sending
     while(!PIR1bits.SSPIF){}
-    
+
     PIR1bits.SSPIF = 0;
     SSPCON2bits.PEN = 1;        //Stop
     while(!PIR1bits.SSPIF){}
 }
 
-int RTC_Recieve(int adresse, int regis)
+char RTC_Recieve(char adresse, char regis)
 {
     PIR1bits.SSPIF = 0;
     SSPCON2bits.SEN = 1;        //START
     while(!PIR1bits.SSPIF){}
-    
+
     PIR1bits.SSPIF = 0;
     SSPBUF = (adresse & 0b11111110);           //Adress write sending
     while(!PIR1bits.SSPIF){}
-    
+
     PIR1bits.SSPIF = 0;
     SSPBUF = regis;           //Registre sending
     while(!PIR1bits.SSPIF){}
-    
+
     PIR1bits.SSPIF = 0;
     SSPCON2bits.RSEN = 1;       //Restart
     while(!PIR1bits.SSPIF){}
-    
+
     PIR1bits.SSPIF = 0;
     SSPBUF = ((adresse & 0b11111110) + 1);           //Adress read sending
     while(!PIR1bits.SSPIF){}
-    
+
     PIR1bits.SSPIF = 0;
     SSPCON2bits.RCEN = 1;          //Allow incoming data
     while(!PIR1bits.SSPIF){}
-    
-    int data = SSPBUF;
-    
+
+    char data = SSPBUF;
+
     PIR1bits.SSPIF = 0;
     SSPCON2bits.PEN = 1;        //Stop
     while(!PIR1bits.SSPIF){}
     
     return data;
 }
+
 // FONCTION DE VERIFICATION ET INITIALISATION RTC
 char Init_RTC (char secondes, char minutes, char heures, char jour, char mois, char annee){
     //VERIFICATION DES VALEURS ENVOYEES
@@ -170,56 +197,62 @@ char Init_RTC (char secondes, char minutes, char heures, char jour, char mois, c
     {
         secondes= secondes%60;
     }
+
     if(minutes>=60)
     {
         minutes= minutes%60;
     }
+
     if(heures>=24)
     {
         heures= heures%24;
     }
+
     if(jour>=31)
     {
         jour= jour%31;
     }
+
     if(mois>=12)
     {
         mois= mois%12;
     }
+
     if(annee >= 100){
         annee = annee%100;
     }
-    // INITIALISATION DES VALEURS CORRIGEES  ET ENVOI A LA RTC 
-    
+
+    // INITIALISATION DES VALEURS CORRIGEES ET ENVOI A LA RTC
     RTC_Send(0b11010000, 0, secondes);
-    RTC_Send (0b11010000, 1, minutes);
-    RTC_Send(0b11010000,2,heures);
-    RTC_Send (0b11010000,4,jour);
+    RTC_Send(0b11010000, 1, minutes);
+    RTC_Send(0b11010000, 2, heures);
+    RTC_Send(0b11010000, 4, jour);
     RTC_Send(0b11010000, 5, mois);
     RTC_Send(0b11010000, 6, annee);
-    ////Initialisation    
-}
 
+}
 
 //FONTION DE LECTURE DE LA RTC
 void Read_RTC(char Tab[6]){
     //LECTURE DANS LES REGISTRES
-    Tab[0] = RTC_Recieve(0b11010000,6);
-    Tab[1]= RTC_Recieve(0b11010000,5);
-    Tab[2]= RTC_Recieve(0b11010000,4);
-    Tab[3]= RTC_Recieve(0b11010000,2);
-    Tab[4]= RTC_Recieve(0b11010000,1);
-    Tab[5]= RTC_Recieve(0b11010000,0);
+    Tab[0] = RTC_Recieve(0b11010000, 6);
+    Tab[1] = RTC_Recieve(0b11010000, 5);
+    Tab[2] = RTC_Recieve(0b11010000, 4);
+    Tab[3] = RTC_Recieve(0b11010000, 2);
+    Tab[4] = RTC_Recieve(0b11010000, 1);
+    Tab[5] = RTC_Recieve(0b11010000, 0);
     
 }
+
 //FONCTIONS DE DECOUPE DES UNITES/DIZAINES
-char min10(char data)
+char get_unit(char data)
 {//DECOUPE UNITES 
     return data & 0b00001111;
 }
-char min60(char data)
+
+char get_dizaine(char data)
 {//DECOUPES DIZAINES 
     return (data >> 4 );
 }
 
-// SI TU LIS CA T ES UN BG <3 
+// SI TU LIS CA T ES UN BG <3
