28baf4c29679d3fdf7c64bb839e07239c71a3f1c
Youri Iacono <47464326+Dvalnir@users.noreply.github.com>
2020-11-19 16:36:07 +0100

    Creation de management.c

---
+++ management.c
@@ -0,0 +1,50 @@
+/* 
+ * File:   main.c
+ * Author: Youri-User
+ *
+ * Created on November 19, 2020, 3:04 PM
+ */
+
+// PIC16F876A Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG
+
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h> 
+
+#define SURVITESSE 50
+#define _XTAL_FREQ 20000000
+
+void main(void) {
+    
+    while(1){
+        if(unsigned char vitesse = get_vitesse(SURVITESSE)){
+            unsigned char tab_RTC[6];
+            Read_RTC(tab_RTC);
+
+            unsigned char tab_EEPROM[8]; // Format : AA   MM   JJ   HH   SS   Entier Vitesse   Decimale Vitesse
+            // Copie la date dans le tableau destiné a l'EEPROM
+            for(unsigned char i = 0; i < 6; i++){
+                tab_EEPROM[i] = tab_RTC[i];
+            }
+            tab_EEPROM[6] = vitesse;
+            tab_EEPROM[7] = 0;
+            
+            Stockage_EEPROM(tab_RTC);
+        }
+    }
+    return;
+}
