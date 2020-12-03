3cfecef4a87ccf2c8c1be5061bc194a3098a97bf
Veysf <florent.veys@student.uclouvain.be>
2020-12-03 12:27:58 +0100

    Clean à test

--- Radar.c
+++ Radar.c
@@ -12,7 +12,7 @@
 #pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
 #pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
 #pragma config CP = OFF  
-// test de modification
+
 #include <xc.h>
 #define _XTAL_FREQ 16000000   // Fréquence de la clock 16MHz
 #define Led4 PORTBbits.RB4       // les Led 4 et 5 sont utiliséent pour des tests
@@ -23,6 +23,12 @@ void sendDataUART(char data);
 char getDataUART();
 char sendData7Seg(char address, char data);
 char waitFlag();
+char scanVitesse();
+void setSurvitesse(char vitesse);
+
+ //variable Global pour radar
+char survitesse = 2;
+char runRadar = 0;
 
 void main(void) {
     
@@ -42,75 +48,22 @@ void main(void) {
     ADCON1 = 6;         //disable analog
     TRISA = 0b00000011;
     TRISB = 0b00000000;    
-    // Déclaration du tableau pour les 7 segments
-    int pos_segment[16]={0b01000100,0b11110101,0b10001100,0b10100100,0b00110101,0b00100110,0b00000110,0b11110100,0b00000100,0b00100100,0b00010100,0b00000111,0b01001110,0b10000101,0b00001110, 0b00011110};
-    // Déclaration du tableau pour choisir les 7 segments
-    char Seg[4] = {0b01000010,0b01000110,0b01000000,0b01001110};
-    // Compteur 
-    char cp = 0;
-    char cp2 = 1;
-    // trame de base
-    char trame[10];
-    char start = 0;
-    // tableau de valeur à envoyer au radar
-    char data[5] = {0b10101010,0b01010101,0b00000000,0b00011000,0b01000001};
-    Led5 = 0;
-    Led4 = 0;
     
-    while(cp < 5){
-    sendDataUART(data[cp]);
-    cp++;
-    __delay_ms(100);
-    }
-    
+    int pos_segment[16]={0b01000100,0b11110101,0b10001100,0b10100100,0b00110101,0b00100110,0b00000110,0b11110100,0b00000100,0b00100100,0b00010100,0b00000111,0b01001110,0b10000101,0b00001110, 0b00011110}; // Déclaration du tableau pour les 7 segments
+    char Seg[4] = {0b01000010,0b01000110,0b01000000,0b01001110}; // Déclaration du tableau pour choisir les 7 segments
+    char data[5] = {0b10101010,0b01010101,0b00000000,0b00011000,0b01000001}; // tableau de valeur à envoyer au radar
+
+    runRadar = 1; // TEST MISE EN ROUTE DU CHECK RADAR
+    char vitesse = 0;
+
     // Boucle infinie
     while(1){
-       sendData7Seg(Seg[2], pos_segment[0]);
-       start = getDataUART(); //Il plante ICI le 2�me passage
-       Led4 = 1;
-       Led5 = 0;
-       if(start == 0xAA){
-           trame[0] = start;
-           //sendData7Seg(Seg[0], pos_segment[trame[0]>>4]);
-           //sendData7Seg(Seg[1], pos_segment[trame[0]& 0b00001111]);
-           while(cp2 < 10){
-               Led4 = 0;
-               Led5 = 1;
-               trame[cp2] = getDataUART();   
-               sendData7Seg(Seg[3], pos_segment[cp2]);
-               if (cp2 == 4 && trame[4] > 0x02){
-                   
-                 sendData7Seg(Seg[0], pos_segment[trame[4]/16]); //dizaine
-                 sendData7Seg(Seg[1], pos_segment[trame[4]%16]); //unit�
-               }
-               Led5 = 0;
-               cp2++;
-           }
-          cp2 = 1;
-           
-           
-           /*
-           Led5 = 1; //DEBUG
-           __delay_ms(100);//DEBUG
-           trame[0] = start;
-           while(cp2 < 10){
-               trame[cp2] = getDataUART();
-               if (cp2 == 4){
-                   sendData7Seg(Seg[1], pos_segment[trame[cp2]]);
-               }
-               cp2++;
-           }
-           
-            * */
-       }
-       else{
-           sendData7Seg(Seg[2], pos_segment[1]);
-       }
-       Led5 = 0;
+        vitesse = scanVitesse();
+        sendData7Seg(Seg[0], pos_segment[vitesse/16]); //dizaine
+        sendData7Seg(Seg[1], pos_segment[vitesse%16]); //unite
     }    
     return;
 }
-
 // Fonction pour envoyer des données au radar
 void sendDataUART(char data){
     
@@ -163,15 +116,37 @@ char waitFlag(){
     }
     return 0;
 }
+//fonction de reception des trames du radar
+char scanVitesse(){
 
+    char trash = 0;
+    char cp2 = 1;
+    char checkSurvitesse = 0;
+    char data = 0;
 
-/*
- TRAME DE CONFIG
- * 
- * 1) 10101010
- * 2) 01010101
- * 3) 00000000
- * 4) 00001010  = 10 
- * 5) 01000000  = 64
- *
- */
+    while(runRadar){
+        sendData7Seg(Seg[2], pos_segment[0]);
+        trash = getDataUART();
+        if(start == 0xAA){
+            trash = start;
+            while(cp2 < 10){
+                trash = getDataUART();   
+                sendData7Seg(Seg[3], pos_segment[cp2]);
+                if (cp2 == 4 && trash > survitesse){  
+                    sendData7Seg(Seg[0], pos_segment[trash/16]); //dizaine
+                    sendData7Seg(Seg[1], pos_segment[trash%16]); //unite
+                    checkSurvitesse = 1;
+                    data = trash;
+               }
+               cp2++;
+           }
+          cp2 = 1;         
+       }
+       if (checkSurvitesse = 1)break;
+    }
+    return data;
+}
+//fonction pour attribuer la valeur de la survitesse
+void setSurvitesse(char vitesse){
+    survitesse = vitesse;
+}
