b48471023f92ad61954d6a0369e63963f4ca1d52
Laurent <lesath190@gmail.com>
2020-12-03 16:03:14 +0100

    FIN

--- Radar.c
+++ Radar.c
@@ -23,12 +23,16 @@ void sendDataUART(char data);
 char getDataUART();
 char sendData7Seg(char address, char data);
 char waitFlag();
-char scanVitesse();
+void scanVitesse();
 void setSurvitesse(char vitesse);
+char tabValueForEEPROM(char tab);
 
  //variable Global pour radar
 char survitesse = 2;
 char runRadar = 0;
+char uniteSurvitesse = 0;
+char decSurvitesse = 0;
+
 
 void main(void) {
     
@@ -52,15 +56,19 @@ void main(void) {
     int pos_segment[16]={0b01000100,0b11110101,0b10001100,0b10100100,0b00110101,0b00100110,0b00000110,0b11110100,0b00000100,0b00100100,0b00010100,0b00000111,0b01001110,0b10000101,0b00001110, 0b00011110}; // Déclaration du tableau pour les 7 segments
     char Seg[4] = {0b01000010,0b01000110,0b01000000,0b01001110}; // Déclaration du tableau pour choisir les 7 segments
     char data[5] = {0b10101010,0b01010101,0b00000000,0b00011000,0b01000001}; // tableau de valeur à envoyer au radar
+    char tabEEPROM[8]; // 
 
     runRadar = 1; // TEST MISE EN ROUTE DU CHECK RADAR
-    char vitesse = 0;
+    
 
     // Boucle infinie
     while(1){
-        vitesse = scanVitesse();
-        sendData7Seg(Seg[0], pos_segment[vitesse/16]); //dizaine
-        sendData7Seg(Seg[1], pos_segment[vitesse%16]); //unite
+        scanVitesse();
+        sendData7Seg(Seg[0], pos_segment[uniteSurvitesse/16]); //dizaine
+        sendData7Seg(Seg[1], pos_segment[uniteSurvitesse%16]); //unite
+        sendData7Seg(Seg[2], pos_segment[decSurvitesse/16]); //dizaine
+        sendData7Seg(Seg[3], pos_segment[decSurvitesse%16]); //unite
+        //tabValueForEEPROM(char tabEEPROM);   ==> a mettre dans la fct d'ecriture vers l'EERPOM
     }    
     return;
 }
@@ -117,12 +125,11 @@ char waitFlag(){
     return 0;
 }
 //fonction de reception des trames du radar
-char scanVitesse(){
+void scanVitesse(){
 
     char trash = 0;
     char cp2 = 1;
     char checkSurvitesse = 0;
-    char data = 0;
 
     while(runRadar){
         trash = getDataUART();
@@ -131,7 +138,10 @@ char scanVitesse(){
                 trash = getDataUART();   
                 if (cp2 == 4 && trash > survitesse){  
                     checkSurvitesse = 1;
-                    data = trash;
+                    uniteSurvitesse = trash;
+               }
+                if (cp2 == 5 && checkSurvitesse){  
+                    decSurvitesse = trash;
                }
                cp2++;
            }
@@ -139,9 +149,15 @@ char scanVitesse(){
        }
        if (checkSurvitesse == 1)break;
     }
-    return data;
 }
 //fonction pour attribuer la valeur de la survitesse
 void setSurvitesse(char vitesse){
     survitesse = vitesse;
+}
+//FONCTION D'ECRITURE DU TABLEAU
+char tabValueForEEPROM(char tab){
+    //Read_RTC(tab);
+    tab[6] = uniteSurvitesse;
+    tab[7] = decSurvitesse;
+    return tab;
 }
